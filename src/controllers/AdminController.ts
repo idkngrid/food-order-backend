4a49619c543c406be649fb6fcd081147a2b2ca64
idkngrid <ingrid.camargopr@gmail.com>
2024-06-20 17:13:17 -0300

    refactor: move vendor validation to a separate method

--- src/controllers/AdminController.ts
+++ src/controllers/AdminController.ts
@@ -1,18 +1,26 @@
 import express, { Request, Response, NextFunction } from 'express';
 import { CreateVendorInput } from '../dto';
 import { Vendor } from '../models';
-import { GeneratePassword, GenerateSalt } from '../utility';
+import { generatePassword, generateSalt } from '../utility';
+
+export const findVendor = async (id: string | undefined, email?: string) => {
+  if(email) {
+    return await Vendor.findOne({ email: email });
+  } else {
+    return await Vendor.findById(id);
+  }
+}
 
 export const createVendor = async (req: Request, res: Response, next: NextFunction) => {
   const { name, ownerName, foodType, pincode, address, phone, email, password } = <CreateVendorInput>req.body;
 
-  const existingVendor = await Vendor.findOne({ email: email });
+  const existingVendor = await findVendor('', email)
   if (existingVendor !== null) {
     return res.json({ "message": "A vendor with this email address already exists." })
   }
 
-  const salt = await GenerateSalt();
-  const userPassword = await GeneratePassword(password, salt);
+  const salt = await generateSalt();
+  const userPassword = await generatePassword(password, salt);
 
   const createdVendor = await Vendor.create({
     name: name, 
@@ -34,9 +42,21 @@ export const createVendor = async (req: Request, res: Response, next: NextFuncti
 }
 
 export const getVendors = async (req: Request, res: Response, next: NextFunction) => {
+  const vendors = await Vendor.find();
+  if (vendors !== null) {
+    return res.json(vendors);
+  }
 
+  return res.json({ "message": "Vendors data not available." })
 }
 
 export const getVendorById = async (req: Request, res: Response, next: NextFunction) => {
+  const id = req.params.id;
+  const vendor = await findVendor(id);
 
+  if (vendor !== null) {
+    return res.json(vendor);
+  }
+
+  return res.json({ "message": "Vendor not found." })
 }
