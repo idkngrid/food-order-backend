ca32eea934f96a33d1a221a14837c211c964cb75
idkngrid <ingrid.camargopr@gmail.com>
2024-06-20 17:11:31 -0300

    feat: add mongoose connection

--- index.ts
+++ index.ts
@@ -1,11 +1,23 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+require('dotenv').config();
+
+import { AdminRoute, VendorRoute } from './src/routes';
+import { MONGO_URI } from './src/config';
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', (req, res) => {
-  return res.json('Hello from the Food Order Backend!')
-})
+app.use('/admin', AdminRoute);
+app.use('/vendor', VendorRoute);
+
+mongoose.connect(MONGO_URI!).then(result => {
+  console.log(result)
+}).catch(err => console.log('error' + err))
 
 app.listen(8000, () => {
-  console.log('App is listeting to port 8000')
+  console.clear();
+  console.log('App is listeting to port 8000');
 })
