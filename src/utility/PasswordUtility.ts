e27de3733d337b32bb5fc738b71b24b93b82446d
idkngrid <ingrid.camargopr@gmail.com>
2024-06-24 18:57:16 -0300

    feat: add generateSignature method to password utils

--- src/utility/PasswordUtility.ts
+++ src/utility/PasswordUtility.ts
@@ -1,4 +1,8 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { AuthPayload, VendorPayload } from '../dto';
+import { APP_SECRET } from '../config';
+import { Request } from 'express';
 
 export const generateSalt = async () => {
   return await bcrypt.genSalt();
@@ -10,4 +14,20 @@ export const generatePassword = async (password: string, salt: string) => {
 
 export const validatePassword = async (enteredPassword: string, savedPassword: string, salt: string) => {
   return await generatePassword(enteredPassword, salt) === savedPassword;
+}
+
+export const generateSignature = (payload: VendorPayload) => {
+  return jwt.sign(payload, APP_SECRET!, { expiresIn: '1d' });
+}
+
+export const validateSignature = async (req: Request) => {
+  const signature = req.get('Authorization');
+
+  if(signature) {
+    const payload = await jwt.verify(signature.split(' ')[1], APP_SECRET!) as unknown as AuthPayload;
+    req.user = payload;
+    return true;
+  }
+
+  return false;
 }
